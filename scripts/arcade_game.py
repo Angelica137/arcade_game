3149ebd959b7975942b68665abdd8ec9e77b21e6
Angelica <Angelica137@users.noreply.github.com>
2022-01-23 20:46:56 +0000

    test score(True, True), green

--- scripts/arcade_game.py
+++ scripts/arcade_game.py
@@ -5,3 +5,9 @@ def eat_ghost(power_pellet_active: bool, touching_ghost: bool) -> bool:
     if power_pellet_active and touching_ghost:
         return True
     return False
+
+
+def score(touching_pallet: bool, touching_dot: bool) -> bool:
+    if touching_pallet or touching_dot:
+        return True
+    return False
