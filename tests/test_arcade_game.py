a80ba217fc2de9b06028b74b30c3416c3bf02131
Angelica <Angelica137@users.noreply.github.com>
2022-01-23 20:41:45 +0000

    test score()m, red

--- tests/test_arcade_game.py
+++ tests/test_arcade_game.py
@@ -5,5 +5,9 @@ def test_eat_ghost_returns_false():
     assert eat_ghost(False, True) == False
 
 
-def test_eat_ghst_returns_true():
+def test_eat_ghost_returns_true():
     assert eat_ghost(True, True) == True
+
+
+def test_score_returns_true():
+    assert score(True, True) == True
