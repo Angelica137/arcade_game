b1a94205656877594ac80b4ed56b92fb5e5045ef
Angelica <Angelica137@users.noreply.github.com>
2022-01-23 20:38:21 +0000

    add type annotations

--- scripts/arcade_game.py
+++ scripts/arcade_game.py
@@ -1,4 +1,7 @@
-def eat_ghost(power_pellet_active, touching_ghost):
+def eat_ghost(power_pellet_active: bool, touching_ghost: bool) -> bool:
+    """
+    Tells us if Pac-Man eats ghost
+    """
     if power_pellet_active and touching_ghost:
         return True
     return False
