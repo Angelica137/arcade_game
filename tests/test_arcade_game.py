98e7db7ba9b9bac53b936b4204f020d674390c0a
Angelica <Angelica137@users.noreply.github.com>
2022-01-23 20:32:21 +0000

    add .eat_ghost(True, True), green

--- tests/test_arcade_game.py
+++ tests/test_arcade_game.py
@@ -3,3 +3,7 @@ from scripts.arcade_game import *
 
 def test_eat_ghost_returns_false():
     assert eat_ghost(False, True) == False
+
+
+def test_eat_ghst_returns_true():
+    assert eat_ghost(True, True) == True
